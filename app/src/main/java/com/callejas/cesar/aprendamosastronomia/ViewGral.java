58882131ac2c248b781f06423fd25a3adacc1471
EDUARDOPC\Eduardo_Chavez <mccartney3150@gmail.com>
2016-10-09 17:05:22 -0600

    Este es diseño base, ojo el color no esta modificado, ya lo arreglare

---
+++ app/src/main/java/com/callejas/cesar/aprendamosastronomia/ViewGral.java
@@ -0,0 +1,13 @@
+package com.callejas.cesar.aprendamosastronomia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewGral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_gral);
+    }
+}
