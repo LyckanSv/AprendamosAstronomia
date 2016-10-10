97dbd359a631e1101988e77d431baa364bfa25ea
EDUARDOPC\Eduardo_Chavez <mccartney3150@gmail.com>
2016-10-10 01:53:16 -0600

    Etapa 1.1 terminada se ve casi igual a la original, ahora se adapta a tabletas me falta hacer que muestre los planetas y los satelites

--- app/src/main/java/com/callejas/cesar/aprendamosastronomia/MainActivity.java
+++ app/src/main/java/com/callejas/cesar/aprendamosastronomia/MainActivity.java
@@ -1,13 +1,111 @@
 package com.callejas.cesar.aprendamosastronomia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<appDatosG> datosGenerales = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarDatos();
     }
+
+
+    public void agregarDatos(){
+        appDatosG ingresoDatos;
+        ingresoDatos = new appDatosG(getString(R.string.tituloSistemaSol),getString(R.string.sistemaSolInfo),R.drawable.universo);
+        datosGenerales.add(ingresoDatos);
+
+        ingresoDatos = new appDatosG(getString(R.string.tituloMercurio),getString(R.string.mercurioInfo),R.drawable.mercurio);
+        datosGenerales.add(ingresoDatos);
+
+        ingresoDatos = new appDatosG(getString(R.string.tituloSatelite1),getString(R.string.satelite1Info),R.drawable.satelites);
+        datosGenerales.add(ingresoDatos);
+
+        ingresoDatos = new appDatosG(getString(R.string.tituloSol),getString(R.string.elSolInfo),R.drawable.sun);
+        datosGenerales.add(ingresoDatos);
+
+        ingresoDatos = new appDatosG(getString(R.string.tituloLuna),getString(R.string.laLunaInfo),R.drawable.moon);
+        datosGenerales.add(ingresoDatos);
+
+        ingresoDatos= new appDatosG(getString(R.string.tituloEstrellas),getString(R.string.estrellasInfo),R.drawable.start);
+        datosGenerales.add(ingresoDatos);
+
+        ingresoDatos = new appDatosG(getString(R.string.tituloConstelacion),getString(R.string.constelacionesInfo),R.drawable.constelaciones);
+        datosGenerales.add(ingresoDatos);
+
+        ingresoDatos = new appDatosG(getString(R.string.tituloNavesEspaciales),getString(R.string.navesEspacialesInfo),R.drawable.nave);
+        datosGenerales.add(ingresoDatos);
+
+        ingresoDatos = new appDatosG(getString(R.string.tituloGalaxias),getString(R.string.galaxiasInfo),R.drawable.galaxia);
+        datosGenerales.add(ingresoDatos);
+    }
+
+    public void sistemaSolar(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(0));
+        startActivity(intent);
+    }
+
+    public void losPlanetas(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(1));
+        startActivity(intent);
+    }
+
+    public void satelitesGrandes(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(2));
+        startActivity(intent);
+    }
+
+    public void elSol(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(3));
+        startActivity(intent);
+    }
+
+    public void laLuna(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(4));
+        startActivity(intent);
+    }
+
+    public void lasEstrellas(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(5));
+        startActivity(intent);
+    }
+
+    public void constelaciones(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(6));
+        startActivity(intent);
+    }
+
+    public void navesEsp(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(7));
+        startActivity(intent);
+    }
+
+
+    public void galaxias(View view){
+        Intent intent = new Intent(this,ViewGral.class);
+        intent.putExtra("datos",datosGenerales.get(8));
+        startActivity(intent);
+    }
+
+
+
+
+
+
 }
